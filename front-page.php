63ec233a43fa5be5c23e78891dc6f8777f305074
Marie-Eve Provost <e9768577@cmaisonneuve.qc.ca>
2022-03-16 19:08:30 -0400

    page principale et animation image

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header() ?>
 <main class="principal">
-    <h1></h1>
-    <!----front-page.php---->
+   
+   
     <section class="animation">
         <div class="animation__bloc">Graphisme</div>
         <div class="animation__bloc">Vidéo</div>
@@ -9,7 +9,7 @@
         <div class="animation__bloc">Sons</div>
         <div class="animation__bloc">Photoshop</div>
     </section>
-
+    <h3>Portefolio de Marie-Eve</h3>
    
             <?php if (have_posts()):the_post();?>
               <?php
