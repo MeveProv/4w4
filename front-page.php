9450418d67f9851a0da126d2457ff17709ff896c
Marie-Eve Provost <e9768577@cmaisonneuve.qc.ca>
2022-03-18 17:17:39 -0400

    s7 TP1 Marie Changement design Menu et animation titre

--- front-page.php
+++ front-page.php
@@ -9,7 +9,7 @@
         <div class="animation__bloc">Sons</div>
         <div class="animation__bloc">Photoshop</div>
     </section>
-    <h4>Portefolio de Marie-Eve</h4>
+    <h4>et le portefolio de Marie-Eve...</h4>
    
             <?php if (have_posts()):the_post();?>
               <?php
