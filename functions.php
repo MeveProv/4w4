30d640e0414c4cb9afdb1477ef6f33ca93b3c9fe
Marie-Eve Provost <e9768577@cmaisonneuve.qc.ca>
2022-04-05 18:42:16 -0400

    s10 c1 implémentation menu accueil

--- functions.php
+++ functions.php
@@ -17,7 +17,8 @@ function cidw_4w4_register_nav_menu(){
         'menu_principal' => __( 'Menu principal', 'cidw_4w4' ),
         'menu_footer'  => __( 'Menu footer', 'cidw_4w4' ),
         'menu_externe' => __('Menu externe', 'cidw_4w4'),
-        'menu_categorie_cours' => __('Menu categorie_cours', 'cidw_4w4')
+        'menu_categorie_cours' => __('Menu categorie_cours', 'cidw_4w4'),
+        'menu_accueil' => __('Menu accueil', 'cidw_4w4')
     ) );
 }
 add_action( 'after_setup_theme', 'cidw_4w4_register_nav_menu', 0 );
