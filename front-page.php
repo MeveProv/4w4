3fe7015851b83489e5d2f77dee43ad5fb22ced33
Marie-Eve Provost <e9768577@cmaisonneuve.qc.ca>
2022-04-05 19:05:46 -0400

    s10 c1 ajout de page et article dans le menu de la page accueil

--- front-page.php
+++ front-page.php
@@ -9,6 +9,11 @@
         <div class="animation__bloc">Sons</div>
         <div class="animation__bloc">Photoshop</div>
     </section>
+    <?php
+    //on appel le menu
+        wp_nav_menu(array("menu"=>"menu_accueil",
+                            "container"=>"nav"));
+    ?>
     <h4>et le portefolio de Marie-Eve...</h4>
    
             <?php if (have_posts()):the_post();?>
