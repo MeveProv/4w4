e0c239916e4249581c8f0f166a842d693409f80a
Marie-Eve Provost <e9768577@cmaisonneuve.qc.ca>
2022-04-19 18:52:22 -0400

    s11 c2 Debut anim frontpage du menu accueil

--- template-evenement.php
+++ template-evenement.php
@@ -28,15 +28,15 @@ $image = get_field('image');
 if( !empty( $image ) ): ?>
     <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 <?php endif; ?>
-<h4>L'endroit</h4>
+<h5>L'endroit</h5>
 <p><?php the_field('endroit'); ?></p>
-<h4>La Date</h4>
+<h5>La Date</h5>
 <p>Date de l'Événement: <?php the_field('date'); ?></p>
-<h4>L'Heure</h4>
+<h5>L'Heure</h5>
 <p>L'heure: <?php the_field('heure'); ?></p>
-<h4>Résumé</h4>
+<h5>Résumé</h5>
 <p>Résumé de l'événement: <?php the_field('resume'); ?></p>
-<h4>Organisateur</h4>
+<h5>Organisateur</h5>
 <p>L'organisteur de l'événement: <?php the_field('organisateur'); ?></p>
 
 <?php
