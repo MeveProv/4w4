e6a1713e648041545964afb08e6f298b02cc1e26
Marie-Eve Provost <e9768577@cmaisonneuve.qc.ca>
2022-04-29 15:05:46 -0400

    s13 c1 Ajout animation css dans bouton burger avec clip path

--- header.php
+++ header.php
@@ -41,9 +41,10 @@
 </header>
 <section class="site__barre" >
     <input type="checkbox"  id="chk-burger">
-    <label for="chk-burger" id="burger">
-        <svg width="34px" height="34px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#ddd"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-    
+    <label for="chk-burger" id="burger" class="burger">
+        <div class="burger__ligne"></div>
+        <div class="burger__ligne"></div>
+        <div class="burger__ligne"></div>
     </label>
     <?php $iconeMenu ='<svg class="iconeMenu" width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0019 16V8a1 1 0 00-1.6-.8l-5.333 4zm-8 0a1 1 0 000 1.6l5.334 4A1 1 0 0011 16V8a1 1 0 00-1.6-.8l-5.334 4z"></path></svg>'
    
