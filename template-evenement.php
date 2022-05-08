37cf10ea3a31eaa3c80db5fd862db806bd0a902e
Marie-Eve Provost <e9768577@cmaisonneuve.qc.ca>
2022-05-08 13:24:21 -0400

    s14 c1 css boite modal

--- template-evenement.php
+++ template-evenement.php
@@ -13,8 +13,8 @@ get_header();
 
 ?>
 <main class="site__main">
-<h1>----------evenement-------</h1>
-<h2>----Ceci est une page example pour template evenement</h2>
+<h1></h1>
+<h2></h2>
 
 <?php if(have_posts()):the_post();?>
 <?php
