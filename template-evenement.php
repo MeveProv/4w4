458034a45634dd9270caf6e4c9aba458c73f3cb4
Marie-Eve Provost <e9768577@cmaisonneuve.qc.ca>
2022-05-08 20:11:13 -0400

    s14 c2 ajout de class pour personnaliser page evenement

--- template-evenement.php
+++ template-evenement.php
@@ -12,9 +12,8 @@
 get_header();
 
 ?>
-<main class="site__main">
-<h1></h1>
-<h2></h2>
+<main class="site__main" id="evenement">
+
 
 <?php if(have_posts()):the_post();?>
 <?php
@@ -22,22 +21,22 @@ the_post_thumbnail();
  
 ?>
 
-<?php the_title();?>
+<h5 class="titreh5">Événements à venir</h5>
 <?php 
 $image = get_field('image');
 if( !empty( $image ) ): ?>
-    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+    <img  class="img_evenement" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 <?php endif; ?>
-<h5>L'endroit</h5>
-<p><?php the_field('endroit'); ?></p>
-<h5>La Date</h5>
-<p>Date de l'Événement: <?php the_field('date'); ?></p>
-<h5>L'Heure</h5>
-<p>L'heure: <?php the_field('heure'); ?></p>
-<h5>Résumé</h5>
-<p>Résumé de l'événement: <?php the_field('resume'); ?></p>
-<h5>Organisateur</h5>
-<p>L'organisteur de l'événement: <?php the_field('organisateur'); ?></p>
+<h5 class="titreh5">L'endroit</h5>
+<p class="endroit"><?php the_field('endroit'); ?></p>
+<h5 class="titreh5">La Date</h5>
+<p class="date">Date de l'Événement: <?php the_field('date'); ?></p>
+<h5 class="titreh5">L'Heure</h5>
+<p class="heure">L'heure: <?php the_field('heure'); ?></p>
+<h5 class="titreh5">Résumé</h5>
+<p class="resume">Résumé de l'événement: <?php the_field('resume'); ?></p>
+<h5 class="titreh5">Organisateur</h5>
+<p class="organisateur">L'organisteur de l'événement: <?php the_field('organisateur'); ?></p>
 
 <?php
     endif;
